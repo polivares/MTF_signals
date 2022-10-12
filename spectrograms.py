56d1dac415274bcf4d7c48310f297bf72b323e46
Patricio Olivares <paolivaresr@gmail.com>
2022-10-11 22:54:46 -0300

    new small update spectrograms.py

--- spectrograms.py
+++ spectrograms.py
@@ -36,7 +36,7 @@ def signals2images(signals, fs, spectrogram = 'spectrogram', img_size = (256, 25
         images.append(signal2spectrogram(signal, fs, 
                                          spectrogram=spectrogram, 
                                          img_size=img_size,
-                                         window=window)
+                                         window=window))
     img_reshape = (len(signals.columns),) + img_size + (1,)
     images = np.array(images).reshape(img_reshape)
     return images
